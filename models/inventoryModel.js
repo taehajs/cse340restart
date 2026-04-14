1ed4cf078dbc4c72dc888c62932869b5bc370a4f
taehajs <taehajs@naver.com>
2026-04-15 00:47:41 +0900

    gasdgasg

--- models/inventoryModel.js
+++ models/inventoryModel.js
@@ -1,4 +1,4 @@
-const pool = require("../database");
+const pool = require("../database/db");
 
 async function getClassifications() {
   return await pool.query(
--- utilities/index.js
+++ utilities/index.js
@@ -1,4 +1,4 @@
-const pool = require("../database");
+const pool = require("../database/db");
 
 
 async function getNav() {
