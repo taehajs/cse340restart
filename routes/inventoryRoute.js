1f9c07963904cc26db793a5ce2fa9f8aeed8f226
taehajs <taehajs@naver.com>
2026-04-14 21:20:02 +0900

    gasdgasd

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,22 +1,15 @@
-const express = require("express");
-const router = new express.Router();
-const invController = require("../controllers/inventoryController");
+const express = require("express")
+const router = express.Router()
+const invController = require("../controllers/inventoryController")
 
-router.get("/", invController.buildManagement);
+router.get("/", invController.buildByClassificationId)
+router.get("/detail/:inv_id", invController.buildByInventoryId)
 
+router.get("/management", invController.buildManagementView)
+router.get("/add-classification", invController.buildAddClassification)
+router.post("/add-classification", invController.addClassification)
 
-router.get("/add-classification", invController.buildAddClassification);
-router.post("/add-classification", invController.addClassification);
+router.get("/add-vehicle", invController.buildAddInventory)
+router.post("/add-vehicle", invController.addInventory)
 
-
-router.get("/add-inventory", invController.buildAddInventory);
-router.post("/add-inventory", invController.addInventory);
-
-router.get("/edit/:invId", invController.buildEditInventory);
-router.post("/update", invController.updateInventory);
-
-
-router.get("/delete/:invId", invController.buildDeleteInventory);
-router.post("/delete", invController.deleteInventory);
-
-module.exports = router;
+module.exports = router
