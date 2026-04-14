091aee71e72ea763819fee6f688391b87a8316f4
taehajs <taehajs@naver.com>
2026-04-14 19:43:53 +0900

    gg

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", (req, res) => {
-  res.render("shared/home", { title: "Home" });
+  res.render("index", { title: "Home" });
 });
 
 module.exports = router;
