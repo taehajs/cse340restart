9b403841dae95c4ded4e30d82df0f2e605776511
taehajs <taehajs@naver.com>
2026-04-15 01:03:08 +0900

    h

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -5,34 +5,36 @@ const utilities = require("../utilities");
 async function buildByClassificationId(req, res, next) {
   try {
     const nav = await utilities.getNav();
-    const classificationList = await inventoryModel.getClassifications();
+    const data = await inventoryModel.getClassifications();
 
     res.render("inventory/index", {
       title: "Inventory Home",
       nav,
-      classificationList: classificationList || { rows: [] }
+      classificationList: data.rows
     });
-
   } catch (err) {
     next(err);
   }
 }
+
 
 async function buildInventoryByClassificationId(req, res, next) {
   try {
     const nav = await utilities.getNav();
-    const data = await inventoryModel.getInventoryByClassificationId(req.params.classificationId);
+    const data = await inventoryModel.getInventoryByClassificationId(
+      req.params.classificationId
+    );
 
     res.render("inventory/classification", {
       title: "Vehicle List",
       nav,
-      vehicles: data.rows || []
+      vehicles: data.rows
     });
-
   } catch (err) {
     next(err);
   }
 }
+
 
 async function buildByInventoryId(req, res, next) {
   try {
@@ -54,39 +56,58 @@ async function buildByInventoryId(req, res, next) {
       nav,
       vehicleHTML
     });
-
   } catch (err) {
     next(err);
   }
 }
 
+
 async function buildAddClassification(req, res, next) {
-  const nav = await utilities.getNav();
-  res.render("inventory/add-classification", { title: "Add Classification", nav });
+  try {
+    const nav = await utilities.getNav();
+
+    res.render("inventory/add-classification", {
+      title: "Add Classification",
+      nav
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 async function addClassification(req, res, next) {
-  const model = require("../models/inventoryModel");
-  await model.addClassification(req.body.classification_name);
-  res.redirect("/inventory");
+  try {
+    await inventoryModel.addClassification(req.body.classification_name);
+    res.redirect("/inventory");
+  } catch (err) {
+    next(err);
+  }
 }
 
 async function buildAddInventory(req, res, next) {
-  const nav = await utilities.getNav();
-  const classificationList = await utilities.buildClassificationList();
+  try {
+    const nav = await utilities.getNav();
+    const classificationList = await utilities.buildClassificationList();
 
-  res.render("inventory/add-inventory", {
-    title: "Add Vehicle",
-    nav,
-    classificationList
-  });
+    res.render("inventory/add-inventory", {
+      title: "Add Vehicle",
+      nav,
+      classificationList
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 async function addInventory(req, res, next) {
-  const model = require("../models/inventoryModel");
-  await model.addInventory(req.body);
-  res.redirect("/inventory");
+  try {
+    await inventoryModel.addInventory(req.body);
+    res.redirect("/inventory");
+  } catch (err) {
+    next(err);
+  }
 }
+
 
 module.exports = {
   buildByClassificationId,
