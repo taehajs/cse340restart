c7996b36e520e3f263a4aa16b98d640beeb4f284
taehajs <taehajs@naver.com>
2026-04-14 21:26:52 +0900

    gasdgasg

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,13 +1,24 @@
+const utilities = require("../utilities/");
+
 async function buildHome(req, res) {
-  res.render("index", { title: "Home", message: "Welcome to the site!" });
+  res.render("index", {
+    title: "Home",
+    nav: await utilities.getNav(),
+  });
 }
 
 async function buildAbout(req, res) {
-  res.render("index", { title: "About", message: "This is the about page." });
+  res.render("index", {
+    title: "About",
+    nav: await utilities.getNav(),
+  });
 }
 
 async function buildContact(req, res) {
-  res.render("index", { title: "Contact", message: "Contact us here." });
+  res.render("index", {
+    title: "Contact",
+    nav: await utilities.getNav(),
+  });
 }
 
 module.exports = { buildHome, buildAbout, buildContact };
