d3901c32aca5c80ce27ea9c242e4fe25fefe979d
taehajs <taehajs@naver.com>
2026-04-14 23:24:22 +0900

    agga

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,24 +2,40 @@ const accountModel = require("../models/accountModel");
 const bcrypt = require("bcryptjs");
 
 exports.buildManagement = async (req, res) => {
-  const account = await accountModel.getById(req.cookies.account_id);
-  res.render("account/management", {
-    title: "Account Management",
-    firstName: account.first_name,
-    accountType: account.account_type,
-    account_id: account.account_id
-  });
+  try {
+    const account = await accountModel.getById(req.cookies.account_id);
+
+    res.render("account/management", {
+      title: "Account Management",
+      firstName: account.first_name,
+      accountType: account.account_type,
+      account_id: account.account_id
+    });
+
+  } catch (err) {
+    res.status(500).render("errors/error", {
+      title: "Error",
+      message: "Failed to load account",
+      nav: ""
+    });
+  }
 };
 
 exports.buildUpdateView = async (req, res) => {
   const account = await accountModel.getById(req.params.id);
-  res.render("account/update", { title: "Update Account", account });
+
+  res.render("account/update", {
+    title: "Update Account",
+    account
+  });
 };
 
 exports.updateAccountInfo = async (req, res) => {
   try {
     await accountModel.updateInfo(req.body);
+
     const account = await accountModel.getById(req.body.account_id);
+
     res.render("account/management", {
       title: "Account Management",
       firstName: account.first_name,
@@ -27,16 +43,24 @@ exports.updateAccountInfo = async (req, res) => {
       account_id: account.account_id,
       message: "Account updated successfully."
     });
+
   } catch (err) {
-    res.render("account/update", { title: "Update Account", account: req.body, errors: [{ msg: "Update failed" }] });
+    res.render("account/update", {
+      title: "Update Account",
+      account: req.body,
+      errors: [{ msg: "Update failed" }]
+    });
   }
 };
 
 exports.updatePassword = async (req, res) => {
   try {
     const hashed = await bcrypt.hash(req.body.password, 10);
+
     await accountModel.updatePassword(req.body.account_id, hashed);
+
     const account = await accountModel.getById(req.body.account_id);
+
     res.render("account/management", {
       title: "Account Management",
       firstName: account.first_name,
@@ -44,8 +68,13 @@ exports.updatePassword = async (req, res) => {
       account_id: account.account_id,
       message: "Password updated successfully."
     });
+
   } catch (err) {
-    res.render("account/update", { title: "Update Account", account: req.body, errors: [{ msg: "Password update failed" }] });
+    res.render("account/update", {
+      title: "Update Account",
+      account: req.body,
+      errors: [{ msg: "Password update failed" }]
+    });
   }
 };
 
@@ -55,4 +84,24 @@ exports.logout = (req, res) => {
   res.clearCookie("firstName");
   res.clearCookie("account_id");
   res.redirect("/");
+};
+
+exports.buildLogin = (req, res) => {
+  res.render("account/login", {
+    title: "Login"
+  });
+};
+
+exports.processLogin = (req, res) => {
+  res.redirect("/account/");
+};
+
+exports.buildRegister = (req, res) => {
+  res.render("account/register", {
+    title: "Register"
+  });
+};
+
+exports.registerAccount = (req, res) => {
+  res.redirect("/account/login");
 };
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
 
+router.get("/login", accountController.buildLogin);
+router.post("/login", accountController.processLogin);
+
+router.get("/register", accountController.buildRegister);
+router.post("/register", accountController.registerAccount);
+
 router.get("/management", accountController.buildManagement);
-router.get("/update/:id", accountController.buildUpdateView);
-router.post("/update", accountController.updateAccountInfo);
-router.post("/update-password", accountController.updatePassword);
-router.get("/logout", accountController.logout);
 
 module.exports = router;
