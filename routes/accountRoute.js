cc766ce5901d2f35496d5188bf8eff6eb84814b1
taehajs <taehajs@naver.com>
2026-04-18 10:51:25 +0900

    fff

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,13 +3,12 @@ const router = new express.Router();
 const accountController = require("../controllers/accountController");
 
 router.get("/login", accountController.buildLogin);
-router.post("/login", accountController.processLogin);
+router.post("/login", accountController.accountLogin); // ⭐ 중요
 
 router.get("/register", accountController.buildRegister);
 router.post("/register", accountController.registerAccount);
 
 router.get("/management", accountController.buildManagement);
-
 router.get("/logout", accountController.logout);
 
 module.exports = router;
