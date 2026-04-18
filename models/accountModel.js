87bb5ceb4b49f83a02cd77703069e8a6a86cdb59
taehajs <taehajs@naver.com>
2026-04-18 18:38:08 +0900

    gggg

--- models/accountModel.js
+++ models/accountModel.js
@@ -1,21 +1,23 @@
 const pool = require("../database/db");
 
-async function getAccountByEmail(email) {
-  const result = await pool.query(
-    "SELECT * FROM account WHERE email = $1",
-    [email]
-  );
-  return result.rows[0];
-}
-
 async function createAccount(email, password) {
-  return pool.query(
-    "INSERT INTO account (email, password, account_type) VALUES ($1, $2, 'Client')",
-    [email, password]
-  );
+  const sql = `
+    INSERT INTO account (
+      first_name,
+      last_name,
+      account_email,
+      account_password,
+      account_type
+    )
+    VALUES ($1, $2, $3, $4, 'Client')
+    RETURNING *;
+  `;
+  return await pool.query(sql, [
+    "User",
+    "User",
+    email,
+    password
+  ]);
 }
 
-module.exports = {
-  getAccountByEmail,
-  createAccount
-};
+module.exports = { createAccount };
